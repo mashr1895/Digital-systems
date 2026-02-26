d6fc24e2d07b02c2c995d35b8e46415ae920d45f
mashr1895 <mashr@post.bgu.ac.il>
2026-02-26 21:21:35 +0200

    Starting to build the Gate class to the Logical Gates objects.

--- LogicalGates.py
+++ LogicalGates.py
@@ -1,45 +1,63 @@
-#### Digial System Course - Logical Gates ###
+#### Digial System Course - Logical Gates Class ###
 #The basic Logical gates - OR,AND,NOT,NOR,NAND,XOR.
 #These will function as the atomic components for us for later in more advanced logical components.
 
+
+
 #OR gate
-def OR(x,y):
+def or_logic(x,y):
     if x == 1 or y == 1:
         return 1
     else:
         return 0
 
 #AND gate
-def AND(x,y):
+def and_logic(x,y):
     if x == 1 and y == 1:
         return 1
     else:
         return 0
 
 #NOT gate
-def NOT(x):
+def not_logic(x):
     if x == 1:
         return 0
     else:
         return 1
 
 #NOR gate
-def NOR(x,y):
-    if OR(x,y) == 1:
+def nor_logic(x,y):
+    if or_logic(x,y) == 1:
         return 0
     else:
         return 1
     
 #NAND gate
-def NAND(x,y):
-    if AND(x,y) == 1:
+def nand_logic(x,y):
+    if and_logic(x,y) == 1:
         return 0
     else:
         return 1
 
-def XOR(x,y):
-    if ((x == 0 and y == 1) or (x == 1 and y == 0)):
+def xor_logic(x,y):
+    if (x == 0 and y == 1):
+        return 1
+    elif (x == 1 and y == 0):
         return 1
     else:
         return 0
 
+
+#### Gate Class ####
+class Gate:
+    def __init__(self,name,num_inputs,logic):
+        self.name = name
+        self.num_inputs = num_inputs
+        self.logic = logic
+
+    def __call__(self,*args):
+        return self.logic(*args)
+
+AND = Gate("AND", 2, and_logic)
+print (AND(1, 0))   # should return 0
+print (AND(1, 1))   # should return 1
