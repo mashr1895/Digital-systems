4437c339deaab29ec5fc4934f8b94f8b27690661
mashr1895 <mashr@post.bgu.ac.il>
2026-02-26 19:34:47 +0200

    First and basic implementation

--- LogicalGates.py
+++ LogicalGates.py
@@ -1,1 +1,45 @@
+#### Digial System Course - Logical Gates ###
+#The basic Logical gates - OR,AND,NOT,NOR,NAND,XOR.
+#These will function as the atomic components for us for later in more advanced logical components.
+
+#OR gate
+def OR(x,y):
+    if x == 1 or y == 1:
+        return 1
+    else:
+        return 0
+
+#AND gate
+def AND(x,y):
+    if x == 1 and y == 1:
+        return 1
+    else:
+        return 0
+
+#NOT gate
+def NOT(x):
+    if x == 1:
+        return 0
+    else:
+        return 1
+
+#NOR gate
+def NOR(x,y):
+    if OR(x,y) == 1:
+        return 0
+    else:
+        return 1
+    
+#NAND gate
+def NAND(x,y):
+    if AND(x,y) == 1:
+        return 0
+    else:
+        return 1
+
+def XOR(x,y):
+    if ((x == 0 and y == 1) or (x == 1 and y == 0)):
+        return 1
+    else:
+        return 0
 
